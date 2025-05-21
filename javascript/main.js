794d56a6bb58724937c966aa046c69d828011be9
SymBiios <julien.sarkarin@gmail.com>
2025-05-21 10:09:46 +0200

    feat: initial commit

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const player1ScoreElement = document.getElementById("player1Score");
+const player2ScoreElement = document.getElementById("player2Score");
+const pointsForVictory = document.getElementById("playingto");
+const scoreLimitInput = document.getElementById("scoreLimit");
+
+function playerOneScore() {}
+
+function playerTwoScore() {}
+
+function resetParty() {}
+
+playerOneBtn.addEventListener("click", playerOneScore);
+playerTwoBtn.addEventListener("click", playerTwoScore);
