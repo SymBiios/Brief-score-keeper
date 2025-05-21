779c814d2f854d4be69dd4dfc1da8eba0ebc1d0d
SymBiios <julien.sarkarin@gmail.com>
2025-05-21 12:15:25 +0200

    feat: add condition for all functions

--- javascript/main.js
+++ javascript/main.js
@@ -1,23 +1,51 @@
 "use strict";
 
-const player1ScoreElement = document.getElementById("player1Score");
-const player2ScoreElement = document.getElementById("player2Score");
-const pointsForVictory = document.getElementById("playingto");
-const scoreLimitInput = document.getElementById("scoreLimit");
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Éléments du DOM:");
+  console.log("player1Score:", document.getElementById("player1Score"));
+  console.log("player2Score:", document.getElementById("player2Score"));
+  console.log("playingTo:", document.getElementById("playingTo"));
+  console.log("scoreLimit:", document.getElementById("scoreLimit"));
+  console.log("playerOneBtn:", document.getElementById("playerOneBtn"));
+  console.log("playerTwoBtn:", document.getElementById("playerTwoBtn"));
+  console.log("resetBtn:", document.getElementById("resetBtn"));
 
-let player1Score = parseInt(player1ScoreElement.textContent);
-let player2Score = parseInt(player2ScoreElement.textContent);
-let VictoryPoints = parseInt(pointsForVictory.textContent);
+  const player1ScoreElement = document.getElementById("player1Score");
+  const player2ScoreElement = document.getElementById("player2Score");
+  const pointsForVictory = document.getElementById("playingTo");
+  const scoreLimitInput = document.getElementById("scoreLimit");
+  const playerOneBtnIncrease = document.getElementById("playerOneBtn");
+  const playerTwoBtnIncrease = document.getElementById("playerTwoBtn");
+  const resetBtn = document.getElementById("resetBtn");
 
-function playerOneScore() {
-  player1Score++;
-  player1ScoreElement.textContent = player1Score;
-}
+  let player1Score = parseInt(player1ScoreElement.textContent);
+  let player2Score = parseInt(player2ScoreElement.textContent);
+  let victoryPoints = parseInt(pointsForVictory.textContent);
 
-function playerTwoScore() {}
+  function playerOneScore() {
+    player1Score++;
+    player1ScoreElement.textContent = player1Score;
+  }
 
-function resetParty() {}
+  function playerTwoScore() {
+    player2Score++;
+    player2ScoreElement.textContent = player2Score;
+  }
 
-playerOneBtn.addEventListener("click", playerOneScore);
-playerTwoBtn.addEventListener("click", playerTwoScore);
-resetBtn.addEventListener("click", resetParty);
+  function resetParty() {
+    player1ScoreElement.textContent = 0;
+    player2ScoreElement.textContent = 0;
+  }
+
+  function whosWinning() {
+    if (
+      player2Score <= scoreLimitInput.textContent ||
+      player1Score <= scoreLimitInput.textContent
+    ) {
+    }
+  }
+
+  playerOneBtnIncrease.addEventListener("click", playerOneScore);
+  playerTwoBtnIncrease.addEventListener("click", playerTwoScore);
+  resetBtn.addEventListener("click", resetParty);
+});
